336248607fef1f71b9ef024d34eb5c3394993106
DuyHung02 <duyhung23102002@gmail.com>
2023-02-14 16:04:43 +0700

    login

---
+++ login/login.js
@@ -0,0 +1,29 @@
+function login() {
+    let account = {
+        "userName": $("#username").val(),
+        "passWord": $("#password").val()
+    }
+
+    $.ajax({
+        type: "Post",
+        headers: {
+            "Accept" : 'application/json',
+            'Content-Type': 'application/json'
+        },
+        url: "http://localhost:8080/login",
+        data: JSON.stringify(account),
+        success: function (data) {
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("user", JSON.stringify(data));
+            alert("thành công")
+            location.href="../index.html";
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+function demo() {
+    alert("alo?")
+}
