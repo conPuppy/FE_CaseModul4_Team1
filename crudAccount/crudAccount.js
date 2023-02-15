46f196f04bd399d5b4d1f4ea4178ce080590a75d
DuyHung02 <duyhung23102002@gmail.com>
2023-02-15 09:40:56 +0700

    up

---
+++ crudAccount/crudAccount.js
@@ -0,0 +1,11 @@
+// function showAccount() {
+//     $.ajax({
+//         type: "Get",
+//         headers: {
+//             'Accept': 'application/json',
+//             'Authorization': 'Bearer ' + localStorage.getItem("token")
+//         },
+//         url: "http://localhost:8080/accounts",
+//         success: function (data)
+//     })
+// }
