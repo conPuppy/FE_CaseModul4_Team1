2b1170a2e187d2f0af7d137137ca6d7e05edd607
DuyHung02 <duyhung23102002@gmail.com>
2023-02-15 09:13:45 +0700

    upRegister

--- login_register/login.js
+++ login_register/login.js
@@ -24,6 +24,11 @@ function login() {
     })
 }
 
+function register() {
+
+}
+
+
 function demo() {
     alert("alo?")
 }
---
+++ login_register/register.js
@@ -0,0 +1,49 @@
+function register(img) {
+    let account = {
+        "userName": $("#reUsername").val(),
+        "passWord": $("#rePassword").val(),
+        "fullName": $("#reFullName").val(),
+        "email": $("#reEmail").val(),
+        "address": $("#reAddress").val(),
+        "phoneNumber": $("#rePhone").val(),
+        "avatar": img,
+        "role": {
+            "id": $("#reRole").val()
+        }
+    }
+
+    $.ajax({
+        type: "Post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        },
+        url: "http://localhost:8080/accounts",
+        data: JSON.stringify(account),
+        success: function (data) {
+            alert("success");
+            location.href="../index.html";
+        }
+    })
+}
+
+function upImg() {
+    let fileImg = document.getElementById("reAvatar").files;
+    let formData = new FormData();
+    formData.append("fileImg", fileImg[0]);
+
+    $.ajax({
+        contentType: false,
+        processData: false,
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        },
+        type: "POST",
+        data: formData,
+        url: "http://localhost:8080/accounts/upImg",
+        success: function (img) {
+            register(img)
+        }
+    });
+}
